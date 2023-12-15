1f01f3948919b9793029040a97a0b3df3b03bf7c
Thinh Ngo <ngongocthinh124@gmail.com>
2023-12-15 11:42:57 +0700

    ok

--- linked-list/LinkedList_FindTheDuplicateNumber.cpp
+++ linked-list/LinkedList_FindTheDuplicateNumber.cpp
@@ -5,6 +5,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution_FastSlowPointers {
+public:
+    /*
+        Trick: Nếu ta coi mỗi 1 vị trí trỏ đến 1 index khác trong num array
+        => Ta traverse như kiểu traverse linked list (node.next)
+
+        Bởi vì điều kiện bài toán là các số đều unique, chỉ có 1 số duy nhất là duplicate
+        => Với mỗi 1 lần traverse với các số unique ta đảm bảo đều reach đến 1 index mới trong array
+
+        Chỉ khi ta gặp số duplicate thì tại vị trí số duplicate sẽ trỏ đến index đã từng lặp qua
+        => Tạ thành 1 cycle linkedlist => Dùng fast và slow pointer để tìm nút thắt
+        
+        Nút thắt chính là duplicate number, vì nút thắt tượng trưng cho 1 số bị trỏ vào 2 lần
+    */
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0;   // index bắt đầu chạy của linkedlist
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        int tmp = 0;
+        while (tmp != slow) {
+            tmp = nums[tmp];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+};
+
 class Solution_BinarySearch {
 public:
     /*
@@ -40,6 +70,7 @@ public:
 
 int main() {
     vector<int> nums = {1,3,4,2,2};
+    cout << Solution_FastSlowPointers().findDuplicate(nums) << "\n";
     cout << Solution_BinarySearch().findDuplicate(nums) << "\n";
     return 0;
 }
